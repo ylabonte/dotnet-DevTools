f1048d90905ec1a45a5b17728100e23fd62ac8f9
Yannic Labonte <yannic.labonte@humbee.de>
2024-02-25 03:29:25 +0100

    [WIP] Refactoring EnvLoaderWindows/SetUserEnvVars --> WindowsEnvLoader

---
+++ WindowsEnvLoader/MainWindow.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace WindowsEnvLoader;
+
+public partial class MainWindow : Form
+{
+    private string _filePath = string.Empty;
+    private Dictionary<string, object>? _envVars = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void newToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        dataGridView.Rows.Clear();
+        dataGridView.Columns.Clear();
+
+        dataGridView.Columns.Add(new DataGridViewColumn(new DataGridViewTextBoxCell())
+        {
+            HeaderText = ProgramResources.VariableName,
+            Name = "Variable",
+            AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells,
+        });
+        dataGridView.Columns.Add(new DataGridViewColumn(new DataGridViewTextBoxCell())
+        {
+            HeaderText = ProgramResources.VariableValue,
+            Name = "Value",
+            AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
+        });
+    }
+
+    private void openToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        using var openFileDialog = new OpenFileDialog();
+        openFileDialog.InitialDirectory = Environment.SpecialFolder.MyDocuments.ToString();
+        openFileDialog.Filter = ProgramResources.OpenFileDialogFilter;
+        openFileDialog.FilterIndex = 2;
+        openFileDialog.RestoreDirectory = true;
+
+        if (openFileDialog.ShowDialog() != DialogResult.OK)
+            return;
+
+        _filePath = openFileDialog.FileName;
+
+        using var reader = new StreamReader(_filePath);
+        var jsonString = reader.ReadToEnd();
+        _envVars = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString);
+
+        dataGridView.Rows.Clear();
+        dataGridView.Columns.Clear();
+
+        dataGridView.Columns.Add(new DataGridViewColumn(new DataGridViewTextBoxCell())
+        {
+            HeaderText = ProgramResources.VariableName,
+            Name = "Variable",
+            AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells,
+        });
+        dataGridView.Columns.Add(new DataGridViewColumn(new DataGridViewTextBoxCell())
+        {
+            HeaderText = ProgramResources.VariableValue,
+            Name = "Value",
+            AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
+        });
+
+        var viewData = from row in _envVars select new[] { row.Key, row.Value.ToString() };
+        foreach (object[] row in viewData)
+        {
+            dataGridView.Rows.Add(row);
+        }
+    }
+
+    private void setToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        foreach (DataGridViewRow row in dataGridView.Rows)
+            if (!string.IsNullOrWhiteSpace((string)row.Cells[0].Value))
+            {
+                RegistryHelper.SetRegistryValue(
+                    Registry.CurrentUser,
+                    "Environment",
+                    (string)row.Cells[0].Value,
+                    row.Cells[1].Value
+                );
+            }
+    }
+
+    private void unsetToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        foreach (DataGridViewRow row in dataGridView.Rows)
+            if (!string.IsNullOrWhiteSpace((string)row.Cells[0].Value))
+            {
+                RegistryHelper.UnsetRegistryValue(
+                    Registry.CurrentUser,
+                    "Environment",
+                    (string)row.Cells[0].Value
+                );
+            }
+    }
+}
---
+++ WindowsEnvLoader/RegistryHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace WindowsEnvLoader;
+
+public static class RegistryHelper
+{
+    /// <summary>
+    /// Open a registry key.
+    /// </summary>
+    /// <param name="baseKey">Registry base/root key (eg. <c>RegistryKey.CurrentUser</c>).</param>
+    /// <param name="registryPath">Registry key as sub path to the given root (eg. <c>@"Software\Adobe\Adobe Acrobat\DC\Privileged"</c>).</param>
+    /// <param name="permissionCheck">optional <c>RegistryKeyPermissionCheck</c> value.</param>
+    /// <returns></returns>
+    public static RegistryKey Open(RegistryKey baseKey, string registryPath,
+        RegistryKeyPermissionCheck permissionCheck = RegistryKeyPermissionCheck.ReadWriteSubTree)
+    {
+        var key = baseKey;
+        var pathParts = new Queue<string?>(registryPath.Split('\\'));
+        while (pathParts.TryDequeue(out var keyName))
+        {
+            if (keyName == null) continue;
+            key = baseKey.OpenSubKey(keyName, permissionCheck);
+            if (key == null)
+                throw new InvalidRegistryPathException(
+                    $"""Invalid registry key: sub key "{keyName}" does not exist.""");
+        }
+
+        return key;
+    }
+
+    /// <summary>
+    /// Overwrite or set a new registry value.
+    /// </summary>
+    /// <param name="root">Registry base/root key (eg. <c>RegistryKey.CurrentUser</c>).</param>
+    /// <param name="registryPath">Registry key as sub path to the given root (eg. <c>@"Software\Adobe\Adobe Acrobat\DC\Privileged"</c>).</param>
+    /// <param name="registryValueName">The registry value name (eg. <c>"bProtectedMode"</c>).</param>
+    /// <param name="value">The actual value to be set.</param>
+    /// <returns>The initial value, in case there was one.</returns>
+    public static object? SetRegistryValue(RegistryKey root, string registryPath, string registryValueName,
+        object value)
+    {
+        object? initialValue = null;
+
+        try
+        {
+            var registryPathKey = Open(root, registryPath);
+            try
+            {
+                initialValue = registryPathKey.GetValue(registryValueName);
+            }
+            catch
+            {
+                // ignored
+            }
+
+            registryPathKey.SetValue(registryValueName, value);
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Unable to " + (initialValue == null ? "set new" : "overwrite") +
+                                $""" registry value ("{registryValueName}").""", e);
+        }
+
+        return initialValue;
+    }
+
+    /// <summary>
+    /// Remove the given registry value.
+    /// </summary>
+    /// <param name="root">Registry base/root key (eg. <c>Microsoft.Win32.RegistryKey.CurrentUser</c>).</param>
+    /// <param name="registryPath">Registry key as sub path to the given root (eg. <c>@"Software\Adobe\Adobe Acrobat\DC\Privileged"</c>).</param>
+    /// <param name="registryValueName">Registry value name (eg. <c>"bProtectedMode"</c>).</param>
+    public static void UnsetRegistryValue(RegistryKey root, string registryPath, string registryValueName)
+    {
+        try
+        {
+            var registryKey = Open(root, registryPath);
+            registryKey.DeleteValue(registryValueName);
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"""Unable to remove registry value ("{registryValueName}").""", e);
+        }
+    }
+}
+
+public class InvalidRegistryPathException(string? message, Exception? innerException = null)
+    : Exception(message, innerException);
